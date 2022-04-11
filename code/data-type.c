be53a19ec6648294db63999b64631c6b91d704ba
wenai sawatwathin <diaz@wenais-MacBook-Air.local>
2022-04-11 07:36:34 +0700

    data-type's done

---
+++ code/data-type.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int age = 40; /* Integer Types */
+    double gpa = 3.0; /* for deciminal. float|double|long double's 6|15|19 decimal places` */
+    char grade = 'A'; /* Integer Types Only 1 Character */
+    char phrase[] = "Giraffe Acdemy"; /* Create Array */
+
+    return (0);
+}
