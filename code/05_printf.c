4c5850355b3bb49f9e67cdd467126039818a87d7
cdxdiaz <wenai.sawatwathin@gmail.com>
2022-05-02 17:30:28 +0700

    05_printf's done

--- code/05_printf.c
+++ code/05_printf.c
@@ -1,18 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
+int     main(void)
 {
-    printf("Hello\nWorld\n");
-    printf("Hello\"World\"\n");
-    printf("My favorite %s is %d\n", "number", 500);
-    printf("My favorite %s is %f\n", "number", 500.0000);
+    printf("My favorite %s is %d\n", "number" , 500);
 
-    int favnum = 90;
-    printf("My favorite %s is %d\n", "number", favnum);
+    printf("My favorite %s is %f\n", "number" , 500.00);
 
-    char mychar = 'i';
-    printf("My favorite %c is %d\n", mychar, favnum);
+    int favNum = 90;
+    printf("My favorite %s is %d\n", "number" , favNum);
+
+    char myChar[] = "NUMBER";
+    printf("My favorite %s is %d\n", myChar , favNum);
 
     return 0;
 }
