be5835d9e5d3485575d31bc17e7b57c3b83d2f20
cdxdiaz <wenai.sawatwathin@gmail.com>
2022-05-08 09:04:24 +0700

    06's done

---
+++ code/06_working-with-numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+int     main()
+{
+    printf("1. %f\n", 8.9);
+    printf("2. %f\n", 5.0 + 4.5);
+    printf("3. %f\n", 5.0 * 4.5);
+    printf("4. %f\n", 5 / 4.0);
+    printf("5. %d\n", 5 / 4);
+    printf("6. %f\n", pow(2, 3));
+    printf("7. %f\n", sqrt(16));
+    printf("8. %f\n", floor(16.9999));
+    
+    return (0);
+}
