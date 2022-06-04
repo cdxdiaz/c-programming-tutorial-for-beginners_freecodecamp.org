16e5d656fc6b04a35c476850074b1442a17b8c0e
cdxdiaz <wenai.sawatwathin@gmail.com>
2022-06-04 19:33:18 +0700

    how to comments

---
+++ code/07_Comments.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    char *s;
+
+    s = "commments";
+    /*THIS IS NOT PRINT*/
+    // THIS IS NOT PRINT TOO
+    printf("%s\n", s);
+}
