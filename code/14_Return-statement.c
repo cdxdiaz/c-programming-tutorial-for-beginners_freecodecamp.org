d588bdbcb325a4bbcf800aabfd9d3390c065edc6
cdxdiaz <wenai.sawatwathin@gmail.com>
2022-06-07 07:36:48 +0700

    session 15- 16

--- code/14_Return-statement.c
+++ code/14_Return-statement.c
@@ -1,2 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+
+double cube(double num)
+{
+    return(num * num * num);
+    printf("Here"); //it's not print !!
+}
+
+int     main(void)
+{
+    printf("Answer: %f", cube(3));
+    return (0);
+}
